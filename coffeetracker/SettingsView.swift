559fae8aa0c5b9433b52e985352cec8816d1506f
Matyáš Strelec <matyasstrelec@gmail.com>
2023-05-30 23:35:40 +0200

    add timer & other fixes

---
+++ coffeetracker/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  coffeetracker
+//
+//  Created by Matyáš Strelec on 30/05/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
